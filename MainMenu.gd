2d5d98b2201cb06fb54182f137385e981b2752d3
Alexandre JUAN <alexandre.juan@epitech.eu>
2021-02-12 21:51:45 +0100

    feat(): add MainMenu and FirstScene
    - CreateMainMenu with Start, Options and Exit choices
    - Create FirstScene (which is an example to play game)
    - Add assets (fonts)

---
+++ MainMenu.gd
@@ -0,0 +1,42 @@
+extends MarginContainer
+
+const firstScene = preload("res://FirstScene.tscn")
+
+onready var selectorOne = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/Selector
+onready var selectorTwo = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/Selector
+onready var selectorThree = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/Selector
+
+var currentSelection = 0
+
+func _ready():
+	setCurrentSelection(0)
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_down") and currentSelection < 2:
+		currentSelection += 1
+		setCurrentSelection(currentSelection)
+	elif Input.is_action_just_pressed("ui_up") and currentSelection > 0:
+		currentSelection -= 1
+		setCurrentSelection(currentSelection)
+	elif Input.is_action_just_pressed("ui_accept"):
+		handleSelection(currentSelection)
+
+func handleSelection(_currentSelection):
+	if _currentSelection == 0:
+		get_parent().add_child(firstScene.instance())
+		queue_free()
+	elif _currentSelection == 1:
+		print("Add Options !")
+	elif _currentSelection == 2:
+		get_tree().quit()
+
+func setCurrentSelection(_currentSelection):
+	selectorOne.text = ""
+	selectorTwo.text = ""
+	selectorThree.text = ""
+	if _currentSelection == 0:
+		selectorOne.text = "> "
+	elif _currentSelection == 1:
+		selectorTwo.text = "> "
+	elif _currentSelection == 2:
+		selectorThree.text = "> "
